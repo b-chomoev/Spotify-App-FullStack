e03c7061b4d7690d67f23636b15212b2ad840f59
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-01-15 23:33:07 -0600

    Added dependencies to front.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { store } from './app/store.ts';
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <ToastContainer/>
+      <App/>
+    </BrowserRouter>
+  </Provider>
+);
