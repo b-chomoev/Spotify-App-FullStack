2616b924174fc1fa4d347e068b0745e8e2ff22e2
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-01-20 00:55:14 -0600

    HW#86 - Added login function.

--- frontend/src/components/UI/AppToolBar/UserMenu.tsx
+++ frontend/src/components/UI/AppToolBar/UserMenu.tsx
@@ -2,40 +2,40 @@ import { Button, Menu, MenuItem } from '@mui/material';
 import { User } from '../../../types';
 import React, { useState } from 'react';
 import { useAppDispatch } from '../../../app/hooks.ts';
-import { unsetUser } from '../../../users/usersSlice.ts';
-import { logout } from '../../../users/usersThunks.ts';
+// import { unsetUser } from '../../../users/usersSlice.ts';
+// import { logout } from '../../../users/usersThunks.ts';
 
-interface Props {
-  user: User;
-}
+// interface Props {
+//   user: User;
+// }
 
 const UserMenu: React.FC<Props> = ({user}) => {
-  const dispatch = useAppDispatch();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleLogOut = () => {
-    dispatch(logout());
-    dispatch(unsetUser());
-  };
+  // const dispatch = useAppDispatch();
+  // const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  //
+  // const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+  //
+  // const handleClose = () => {
+  //   setAnchorEl(null);
+  // };
+  //
+  // const handleLogOut = () => {
+  //   dispatch(logout());
+  //   dispatch(unsetUser());
+  // };
 
   return (
     <>
-      <Button color='inherit' onClick={handleClick}>
+      <Button color='inherit' >
         Hello, {user.username}
       </Button>
-      <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-        <MenuItem>Profile</MenuItem>
-        <MenuItem>My Account</MenuItem>
-        <MenuItem onClick={handleLogOut}>Log Out</MenuItem>
-      </Menu>
+      {/*<Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>*/}
+      {/*  <MenuItem>Profile</MenuItem>*/}
+      {/*  <MenuItem>My Account</MenuItem>*/}
+      {/*  <MenuItem onClick={handleLogOut}>Log Out</MenuItem>*/}
+      {/*</Menu>*/}
     </>
   );
 };
--- frontend/src/users/usersSlice.ts
+++ frontend/src/users/usersSlice.ts
@@ -1,23 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { User, ValidationError } from '../types';
-import { register } from './usersThunks.ts';
+import { GlobalError, User, ValidationError } from '../types';
+import { login, register } from './usersThunks.ts';
 import { RootState } from '../app/store.ts';
 
 interface UsersState {
   user: User | null;
   registerLoading: boolean;
   registerError: ValidationError | null;
+  loginLoading: boolean;
+  loginError: GlobalError | null;
 }
 
 const initialState: UsersState = {
   user: null,
   registerLoading: false,
   registerError: null,
+  loginLoading: false,
+  loginError: null,
 }
 
 export const selectUser = (state: RootState) => state.users.user;
 export const selectRegisterLoading = (state: RootState) => state.users.registerLoading;
 export const selectRegisterError = (state: RootState) => state.users.registerError;
+
+export const selectLoginError = (state: RootState) => state.users.loginError;
+export const selectLoginLoading = (state: RootState) => state.users.loginLoading;
 
 export const usersSlice = createSlice({
   name: 'users',
@@ -37,6 +44,19 @@ export const usersSlice = createSlice({
         state.registerLoading = false;
         state.registerError = error || null;
       })
+
+      .addCase(login.pending, (state) => {
+        state.loginLoading = true;
+        state.loginError = null;
+      })
+      .addCase(login.fulfilled, (state, {payload: user}) => {
+        state.loginLoading = false;
+        state.user = user;
+      })
+      .addCase(login.rejected, (state, {payload: error}) => {
+        state.loginLoading = false;
+        state.loginError = error || null;
+      });
   }
 });
 
