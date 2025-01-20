1d937585221a3c9cf4113c6e1ea291d10b8e32fc
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-01-20 01:54:43 -0600

    HW#86 - Albums are going to be displayed only logged in user.

--- frontend/src/features/artists/containers/Artists.tsx
+++ frontend/src/features/artists/containers/Artists.tsx
@@ -5,8 +5,10 @@ import { CircularProgress, Typography } from '@mui/material';
 import ArtistItem from '../components/ArtistItem.tsx';
 import { useEffect } from 'react';
 import { fetchArtists } from '../artistsThunks.ts';
+import { selectUser } from '../../../users/usersSlice.ts';
 
 const Artists = () => {
+  const user = useAppSelector(selectUser);
   const dispatch = useAppDispatch();
   const artists = useAppSelector(selectArtists);
   const isFetchArtistsLoading = useAppSelector(selectFetchLoading);
@@ -16,33 +18,42 @@ const Artists = () => {
   }, [dispatch]);
 
   return (
-    <Grid container direction={'column'} spacing={2}>
-      <Grid container justifyContent="space-between" alignItems="center">
-        <Grid>
-          <Typography variant="h4">Artists</Typography>
-        </Grid>
-      </Grid>
+    <>
+      {user ?
+        <>
+          <Grid container direction={'column'} spacing={2}>
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid>
+                <Typography variant="h4">Artists</Typography>
+              </Grid>
+            </Grid>
 
-      <Grid container direction="row" spacing={1}>
+            <Grid container direction="row" spacing={1}>
 
-        {isFetchArtistsLoading ? <CircularProgress /> :
-          <>
-            {artists.length === 0 && !isFetchArtistsLoading ? <Typography variant="h6">No artists yet</Typography> :
-              <>
-                {artists.map(artist => (
-                  <ArtistItem
-                    key={artist._id}
-                    id={artist._id}
-                    name={artist.name}
-                    image={artist.image}
-                  />
-                ))}
-              </>
-            }
-          </>
-        }
-      </Grid>
-    </Grid>
+              {isFetchArtistsLoading ? <CircularProgress/> :
+                <>
+                  {artists.length === 0 && !isFetchArtistsLoading ?
+                    <Typography variant="h6">No artists yet</Typography> :
+                    <>
+                      {artists.map(artist => (
+                        <ArtistItem
+                          key={artist._id}
+                          id={artist._id}
+                          name={artist.name}
+                          image={artist.image}
+                        />
+                      ))}
+                    </>
+                  }
+                </>
+              }
+            </Grid>
+          </Grid>
+        </>
+        :
+        <Typography variant='h3'>Hi! You have to Sign Up or Log In first!</Typography>
+      }
+    </>
   );
 };
 
